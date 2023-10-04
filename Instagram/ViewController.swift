2161eb660146da8b815a8b78ced01510b8845604
okami-ap <y.okami@alphapolis.co.jp>
2023-10-05 07:07:04 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by WEBSYSTEM-MAC29 on 2023/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
