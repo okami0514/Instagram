9db06c3d822a1430523a92bd4420bd1ddbf5515c
okami-ap <y.okami@alphapolis.co.jp>
2023-10-11 14:56:22 +0900

    画像表示処理実装

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -6,13 +6,12 @@
 //
 
 import UIKit
+import CLImageEditor
 
-class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func handleLibraryButton(_ sender: Any) {
@@ -48,12 +47,32 @@ class ImageSelectViewController: UIViewController, UIImagePickerControllerDelega
         if info[.originalImage] != nil {
             // 撮影/選択された画像を取得する
             let image = info[.originalImage] as! UIImage
-            // あとでCLImageEditorライブラリで加工する
+            
+            // CLImageEditorライブラリで加工する
             print("DEBUG_PRINT: image = \(image)")
+            // CLImageEditorにimageを渡して、加工画面を起動する。
+            let editor = CLImageEditor(image: image)!
+            editor.delegate = self
+            self.present(editor, animated: true, completion: nil)
         }
     }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // UIImagePickerController画面を閉じる
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // CLImageEditorで加工が終わったときに呼ばれるメソッド
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        // 投稿画面を開く
+        let postViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! PostViewController
+        postViewController.image = image!
+        editor.present(postViewController, animated: true, completion: nil)
+    }
+    
+    // CLImageEditorの編集がキャンセルされた時に呼ばれるメソッド
+    func imageEditorDidCancel(_ editor: CLImageEditor!) {
+        // CLImageEditor画面を閉じる
+        editor.dismiss(animated: true, completion: nil)
     }
 }
